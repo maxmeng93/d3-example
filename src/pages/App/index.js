66bc39e8596a3805f16f92fda333a947c33339cc
Meng YuCheng <maxmeng93@gmail.com>
2019-07-25 13:49:48 +0800

    update

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import './index.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      123
+    <Router>
+      <div>
+        <Header></Header>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/topics" component={Topics}></Route>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topic({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>;
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li><Link to={`${match.url}/components`}>Components</Link></li>
+        <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
+      </ul>
+      <Route path={`${match.path}/:id`} component={Topic}></Route>
+      <Route exact path={match.path} render={() => <h3>Please select a topic.</h3>}></Route>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/topics">Topics</Link></li>
+    </ul>
   );
 }
 
