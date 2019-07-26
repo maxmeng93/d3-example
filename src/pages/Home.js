4edc5579337b96ca60c210435608fa643a4f3d93
Meng YuCheng <maxmeng93@gmail.com>
2019-07-26 17:05:00 +0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ import Charts from '../components/Charts';
 
 function App() {
   return (
-    <>
+    <div class="content">
       <h2>首页</h2>
       <Charts></Charts>
-    </>
+    </div>
   );
 }
 
