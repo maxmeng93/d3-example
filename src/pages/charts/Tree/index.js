9bab7fc2e84644a2c376823e75332afed2f5151c
Meng YuCheng <maxmeng93@gmail.com>
2019-08-02 11:06:39 +0800

    update

---
+++ src/pages/charts/Tree/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import * as d3 from 'd3';
+
+class Line extends React.Component {
+  render() {
+    return (
+      <div className="content">
+        <span><Link to="/">Home</Link></span>
+        <h1>树图</h1>
+      </div>
+    );
+  }
+}
+
+export default Line;
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,17 @@ import Regist from '../pages/user/Regist';
 // 图表
 import Line from '../pages/charts/Line';
 import Bar from '../pages/charts/Bar';
+import Pie from '../pages/charts/Pie';
+import Scatter from '../pages/charts/Scatter';
+import Radar from '../pages/charts/Radar';
+import Tree from '../pages/charts/Tree';
+import Treemap from '../pages/charts/Treemap';
+import Sunburst from '../pages/charts/Sunburst';
+import Boxplot from '../pages/charts/Boxplot';
+import Candlestick from '../pages/charts/Candlestick';
+import Heatmap from '../pages/charts/Heatmap';
+import Maps from '../pages/charts/Map';
+import Graph from '../pages/charts/Graph';
 
 export default () => (
   <Router>
@@ -19,6 +30,17 @@ export default () => (
       <Route path="/regist" component={Regist} />
       <Route path="/line" component={Line} />
       <Route path="/bar" component={Bar} />
+      <Route path="/pie" component={Pie} />
+      <Route path="/scatter" component={Scatter} />
+      <Route path="/radar" component={Radar} />
+      <Route path="/tree" component={Tree} />
+      <Route path="/treemap" component={Treemap} />
+      <Route path="/sunburst" component={Sunburst} />
+      <Route path="/boxplot" component={Boxplot} />
+      <Route path="/candlestick" component={Candlestick} />
+      <Route path="/heatmap" component={Heatmap} />
+      <Route path="/map" component={Maps} />
+      <Route path="/graph" component={Graph} />
       <Route component={Error404} />
     </Switch>
   </Router>
