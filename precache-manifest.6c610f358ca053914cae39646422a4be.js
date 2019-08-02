520ff34c4527803e92bdb74bc3e2da56fd6e258b
Meng YuCheng <maxmeng93@gmail.com>
2019-08-02 16:57:18 +0800

    Updates

--- precache-manifest.6c610f358ca053914cae39646422a4be.js
+++ precache-manifest.6c610f358ca053914cae39646422a4be.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f9dd6a545ffe0814e94860fdfff5071",
+    "revision": "245870a450a3c5cf0755efd3f9c9a8c1",
     "url": "/d3-example/index.html"
   },
   {
-    "revision": "3533575b0b9447c3ddd2",
+    "revision": "1d19cc0e6e2539640887",
     "url": "/d3-example/static/css/2.ad187f9c.chunk.css"
   },
   {
-    "revision": "8b6e844bf6401ad6b9a8",
-    "url": "/d3-example/static/css/main.05e732a0.chunk.css"
+    "revision": "58961f088b6715d60abe",
+    "url": "/d3-example/static/css/main.1ada1012.chunk.css"
   },
   {
-    "revision": "3533575b0b9447c3ddd2",
-    "url": "/d3-example/static/js/2.0f0b4d57.chunk.js"
+    "revision": "1d19cc0e6e2539640887",
+    "url": "/d3-example/static/js/2.4df04278.chunk.js"
   },
   {
-    "revision": "8b6e844bf6401ad6b9a8",
-    "url": "/d3-example/static/js/main.39663497.chunk.js"
+    "revision": "58961f088b6715d60abe",
+    "url": "/d3-example/static/js/main.65a6536c.chunk.js"
   },
   {
     "revision": "41fc674bed8e98925a8f",
@@ -26,6 +26,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "10b5edf583ce470da3afa4b8fb2f8f9e",
     "url": "/d3-example/static/media/index.10b5edf5.csv"
+  },
+  {
+    "revision": "38e4271f9199c25b18bbc6a0aa1918ee",
+    "url": "/d3-example/static/media/index.38e4271f.md"
+  },
+  {
+    "revision": "6229f99ad2f3eca595ad14b06c58e964",
+    "url": "/d3-example/static/media/index.6229f99a.md"
   },
   {
     "revision": "78a8f568eeb45420cd61072120c21f81",
@@ -38,6 +46,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b82b781c3cbbf5d25d7693f74c441130",
     "url": "/d3-example/static/media/index.b82b781c.md"
+  },
+  {
+    "revision": "8c643b9c0bc65d99d0a9330e6ffcac59",
+    "url": "/d3-example/static/media/life.8c643b9c.txt"
   },
   {
     "revision": "5b5b12f007ae3930e222757e22e4aeaa",
